fbe8e436535330a2e18eb05aa8b324b7146f8569
geppetto <null.geppetto@gmail.com>
2016-12-05 01:25:12 +0200

    Add aux.py (batch_iterator function added)

---
+++ aux.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def iterate_minibatches(inputs, targets, batch_size, shuffle=False):
+    """
+    Batch iterator
+    """
+    assert len(inputs) == len(targets)
+    if shuffle:
+        indices = np.arange(len(inputs))
+        np.random.shuffle(indices)
+    for start_idx in range(0, len(inputs) - batch_size + 1, batch_size):
+        if shuffle:
+            excerpt = indices[start_idx:start_idx + batch_size]
+        else:
+            excerpt = slice(start_idx, start_idx + batch_size)
+        yield inputs[excerpt], targets[excerpt]
