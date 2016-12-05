23ca1548e88b575f897ce9aa2647ac5418c5fa9c
geppetto <null.geppetto@gmail.com>
2016-12-05 16:50:47 +0200

    Update cnn_models.py (build_ccfff_model)

---
+++ cnn_models.py
@@ -0,0 +1,62 @@
+import lasagne
+from lasagne.nonlinearities import rectify as relu
+from lasagne.nonlinearities import softmax, sigmoid, softplus
+from lasagne.layers import InputLayer, Conv2DLayer, Pool2DLayer, DenseLayer, dropout, GaussianNoiseLayer
+
+
+def build_ccfff_model(input_var=None, data_shape=None):
+    """
+    ---------------------
+    Architecture: "CCFFF"
+    ---------------------
+        Input Layer
+        ConvLayer  : 64 filters, 3x3
+        MaxPooling : 2x2
+        ConvLayer  : 128 filters, 3x3
+        MaxPooling : 2x2
+        FCLayer    : 256 units (+ReLU)
+        FCLayer    : 256 units (+ReLU)
+        FCLayer    :  10 units (+Soft-max) [Output Layer]
+    """
+    # Input layer
+    network = InputLayer(shape=data_shape, input_var=input_var)
+
+    # 1st convolution layer
+    network = Conv2DLayer(network,
+                          num_filters=64,
+                          filter_size=(3, 3),
+                          nonlinearity=relu,
+                          W=lasagne.init.GlorotUniform(),
+                          name='conv_1')
+    # 1st pooling layer
+    network = Pool2DLayer(network, pool_size=(2, 2), mode='max', name='pool_1')
+
+    # 2nd convolution layer
+    network = Conv2DLayer(network,
+                          num_filters=128,
+                          filter_size=(3, 3),
+                          nonlinearity=lasagne.nonlinearities.rectify,
+                          W=lasagne.init.GlorotUniform(),
+                          name='conv_2')
+    # 2nd pooling layer
+    network = Pool2DLayer(network, pool_size=(2, 2), mode='max', name='pool_2')
+
+    # 1st Fully-connected layer
+    network = DenseLayer(incoming=dropout(network, p=0.5),
+                         num_units=256,
+                         nonlinearity=relu,
+                         name='fc_1')
+
+    # 2nd Fully-connected layer
+    network = DenseLayer(incoming=dropout(network, p=0.5),
+                         num_units=256,
+                         nonlinearity=relu,
+                         name='fc_2')
+
+    # Output layer
+    network = DenseLayer(network,
+                         num_units=10,
+                         nonlinearity=softmax,
+                         name='output')
+
+    return network
