b3d1bbf0f23aaa58caa8fac810a5e79dc78449fa
geppetto <null.geppetto@gmail.com>
2016-12-05 16:49:55 +0200

    Update aux.py (create_trained_models_dir)

--- aux.py
+++ aux.py
@@ -1,4 +1,6 @@
 import numpy as np
+import lasagne
+import os
 
 
 def iterate_minibatches(inputs, targets, batch_size, shuffle=False):
@@ -15,3 +17,14 @@ def iterate_minibatches(inputs, targets, batch_size, shuffle=False):
         else:
             excerpt = slice(start_idx, start_idx + batch_size)
         yield inputs[excerpt], targets[excerpt]
+
+
+def create_trained_models_dir(network, model_filename):
+    """
+
+    """
+    trained_models_dir = "./trained_models"
+    if not os.path.exists(trained_models_dir):
+        os.makedirs(trained_models_dir)
+
+    np.savez(model_filename, *lasagne.layers.get_all_param_values(network))
