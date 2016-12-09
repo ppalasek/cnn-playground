5707c8b788b234854c58a44e56f13850d2555b63
geppetto <null.geppetto@gmail.com>
2016-12-09 15:01:52 +0200

    Remove the  arguent; embed it into

--- runCNN.py
+++ runCNN.py
@@ -8,7 +8,7 @@ import theano
 import theano.tensor as T
 import lasagne
 from aux import iterate_minibatches
-from read_data import load_cifar10
+from read_data import load_mnist, load_cifar10, load_cifar100, load_svhn
 from cnn_models import build_ccfff_model, build_ccffsvm_model
 
 
@@ -21,8 +21,9 @@ def main():
         - CIFAR-100
         - SVHN
     based on one of the following architectures:
-        - 'ccfff' : A simple architecture consisting of 2 convolution and 3 fully-
-                    connected layers (see `build_ccfff_model()` in `cnn_models.py`)
+        - 'ccfff'   : A simple architecture consisting of 2 convolution and 3 fully-
+                      connected layers (see `build_ccfff_model()` in `cnn_models.py`)
+        - 'ccffsvm' : Similar to 'ccfff' ...
         - To be added more architectures (like ResNet, WRN, VGG)
 
     Run `runCNN.py -h` for more details on command line arguments.
@@ -45,7 +46,6 @@ def main():
     parser.add_argument("-a", "--architecture", type=str, choices=['ccfff', 'ccffsvm'], default="ccffsvm", help="choose CNN architecture")
     parser.add_argument("-e", "--num_epochs", type=int, default=100, help="set number of epochs")
     parser.add_argument("-b", "--batch_size", type=int, default=256, help="set batch size")
-    parser.add_argument("-l", '--loss', type=str, choices=['cross_entropy', 'hinge'], default="cross_entropy", help="choose loss (objective) function")
     parser.add_argument("-s", "--save_model", action="store_true", help="save model (weights)")
     args = parser.parse_args()
 
@@ -55,13 +55,18 @@ def main():
         print("    -- Architecture     : %s" % args.architecture)
         print("    -- Number of epochs : %d" % args.num_epochs)
         print("    -- Batch size       : %d" % args.batch_size)
-        print("    -- Loss function    : %s" % args.loss)
 
     # Load the dataset
     print(" # Loading data...", end="")
     sys.stdout.flush()
     if args.dataset == "mnist":
-        raise NotImplementedError
+        # Load MNIST dataset
+        X_train, y_train, X_val, y_val, X_test, y_test = load_mnist()
+        data_shape = (None, 1, 28, 28)
+        # Prepare Theano variables for inputs and targets
+        input_var = T.tensor4('inputs')
+        target_var = T.ivector('targets')
+
     elif args.dataset == "cifar10":
         # Load dataset
         X_train, y_train, X_val, y_val, X_test, y_test = load_cifar10()
@@ -69,8 +74,10 @@ def main():
         # Prepare Theano variables for inputs and targets
         input_var = T.tensor4('inputs')
         target_var = T.ivector('targets')
+
     elif args.dataset == "cifar100":
         raise NotImplementedError
+
     elif args.dataset == "svhn":
         raise NotImplementedError
     print("Done!")
@@ -85,15 +92,10 @@ def main():
     elif args.architecture == "":
         raise NotImplementedError
 
-    if args.verbose:
-        print(" # Number of parameters in model: %d"
-              % lasagne.layers.count_params(network, trainable=True))
-
-    if (args.architecture == "ccffsvm"):
+    if args.architecture == "ccffsvm":
         scores = lasagne.layers.get_output(network)
         loss = network.get_one_vs_all_cost_from_scores(scores, target_var)
 
-        prediction = network.get_class_from_scores(scores)
     else:
         # Create a loss expression for training
         prediction = lasagne.layers.get_output(network)
@@ -112,9 +114,10 @@ def main():
     # Create update expressions for training
     # Stochastic Gradient Descent (SGD) with momentum
     params = lasagne.layers.get_all_params(network, trainable=True)
-    lr = 0.1
-    if (args.architecture == "ccffsvm"):
+    if args.architecture == "ccffsvm":
         lr = 0.01
+    else:
+        lr = 0.1
 
     sh_lr = theano.shared(lasagne.utils.floatX(lr))
     updates = lasagne.updates.momentum(loss, params, learning_rate=sh_lr, momentum=0.9)
@@ -124,23 +127,18 @@ def main():
     train_fn = theano.function([input_var, target_var], loss, updates=updates)
 
     # Create a loss expression for validation/testing
-    if (args.architecture == "ccffsvm"):
+    if args.architecture == "ccfff":
+        test_prediction = lasagne.layers.get_output(network, deterministic=True)
+        test_loss = lasagne.objectives.categorical_crossentropy(test_prediction, target_var)
+        test_loss = test_loss.mean()
+        test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
+                          dtype=theano.config.floatX)
+
+    elif args.architecture == "ccffsvm":
         scores = lasagne.layers.get_output(network, deterministic=True)
         test_loss = network.get_one_vs_all_cost_from_scores(scores, target_var)
-
         test_prediction = network.get_class_from_scores(scores)
-
         test_acc = T.mean(T.eq(test_prediction, target_var),
-                          dtype=theano.config.floatX)
-    else:
-        test_prediction = lasagne.layers.get_output(network, deterministic=True)
-        if args.loss == "cross_entropy":
-            test_loss = lasagne.objectives.categorical_crossentropy(test_prediction, target_var)
-        if args.loss == "hinge":
-            test_loss = lasagne.objectives.multiclass_hinge_loss(test_prediction, target_var)
-        test_loss = test_loss.mean()
-
-        test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
                           dtype=theano.config.floatX)
 
     # Compile a second function computing the validation loss and accuracy:
@@ -148,6 +146,8 @@ def main():
 
     # Done building network and compiling functions
     print("Done!")
+    print(" # Number of parameters in model: %d" %
+          lasagne.layers.count_params(network, trainable=True))
 
     # Start training
     print(" # Starting training...", end="")
@@ -176,33 +176,36 @@ def main():
             val_batches += 1
 
         # Then we print the results for this epoch:
-        print("Epoch {} of {} took {:.3f}s".format(
-            epoch + 1, args.num_epochs, time.time() - start_time))
-        print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
-        print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
-        print("  validation accuracy:\t\t{:.2f} %".format(
-            val_acc / val_batches * 100))
+        if args.verbose:
+            print("Epoch {} of {} took {:.3f}s".format(
+                epoch + 1, args.num_epochs, time.time() - start_time))
+            print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
+            print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
+            print("  validation accuracy:\t\t{:.2f} %".format(
+                val_acc / val_batches * 100))
 
-        # Adjust learning rate (after 41 and 61 epochs)
-        if (epoch + 1) == 41 or (epoch + 1) == 61:
-            new_lr = sh_lr.get_value() * 0.1
-            if args.verbose:
-                print("New LR:" + str(new_lr))
-            sh_lr.set_value(lasagne.utils.floatX(new_lr))
+        # # Adjust learning rate (after 41 and 61 epochs)
+        # if (epoch + 1) == 41 or (epoch + 1) == 61:
+        #     new_lr = sh_lr.get_value() * 0.1
+        #     if args.verbose:
+        #         print("New LR:" + str(new_lr))
+        #     sh_lr.set_value(lasagne.utils.floatX(new_lr))
 
     # Save trained model
     if args.save_model:
+        if args.verbose:
+            print(" # Save trained model...", end="")
         # Create trained model dir
         trained_models_dir = "./trained_models"
         if not os.path.exists(trained_models_dir):
             os.makedirs(trained_models_dir)
         # Trained model filename format:
-        # <dataset>_<architecture>_<num_epochs>_<batch_size>_<loss>_model.npz
-        model_filename = "%s/%s_%s_%d_%d_%s_model.npz" % \
-                         (trained_models_dir, args.dataset,
-                          args.architecture, args.num_epochs,
-                          args.batch_size, args.loss)
+        # <dataset>_<architecture>_<num_epochs>_<batch_size>_model.npz
+        model_filename = "%s/%s_%s_%d_%d_model.npz" % \
+                         (trained_models_dir, args.dataset, args.architecture,
+                          args.num_epochs, args.batch_size)
         np.savez(model_filename, *lasagne.layers.get_all_param_values(network))
+        print("Done!")
 
     # Calculate validation error of model:
     test_err = 0
@@ -214,9 +217,9 @@ def main():
         test_err += err
         test_acc += acc
         test_batches += 1
-    print("Final results:")
-    print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
-    print("  test accuracy:\t\t{:.2f} %".format(test_acc / test_batches * 100))
+    print(" # Final results:")
+    print("   test loss:\t\t\t{:.6f}".format(test_err / test_batches))
+    print("   test accuracy:\t\t{:.2f} %".format(test_acc / test_batches * 100))
 
 
 if __name__ == "__main__":
